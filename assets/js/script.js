135fc448c24bd9f415f09c9da4f39a02f9fa7c3d
JJBR13 <jjbrown1308@gmail.com>
2022-09-27 14:28:13 +0000

    Create startGame() & naration object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log('im here');
+//* Start game *// 
+
+const startButton = document.getElementById('start-btn');
+const storyContainer = document.getElementById('story-segment')
+
+startButton.addEventListener('click', startGame);
+
+function startGame () {
+    startButton.classList.add('hide');
+    storyContainer.classList.remove('hide');
+}
--- assets/js/story.js
+++ assets/js/story.js
@@ -1,1 +1,11 @@
-console.log('im here');
+//* Story objects *// 
+
+const stories = [
+    { narration1: "In the light of the moon a little egg lay on a leaf. One Sunday morning the warm sun came up- crack!",
+
+     option1: "The egg was hatching",
+     option2: "The leaf fell from the tree"
+
+    }
+        
+]
