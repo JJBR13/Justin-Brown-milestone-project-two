94fb4a016546bc07498cb53a481a579846b04530
JJBR13 <jjbrown1308@gmail.com>
2022-09-30 09:25:22 +0000

    For loops and functions added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,57 @@
-//* Start game *// 
+// Start game 
 
+const optionContainer = document.getElementById('answer-btn')
 const startButton = document.getElementById('start-btn')
+const selectButton = document.getElementsByClassName('op-btn')
 const narrationStoryLine = document.getElementById('story-segment')
-const optionButtons = document.getElementById('answer-btn')
+
+
+let currentStoryIndex = 0;
+let selectOptions = [];
 
 startButton.addEventListener('click', startGame);
 
 function startGame () {
     startButton.classList.add('hide');
     narrationStoryLine.classList.remove('hide');
-    optionButtons.classList.remove('hide');
+    optionContainer.classList.remove('hide');
 }
 
-/* Displaying start Story */ 
+// Displaying start Story 
 
 function showNarrations(object) {
     console.log(object.narration1)
-    narrationStoryLine.innerText = object.narration1    
+    narrationStoryLine.innerText = object.narration
 }
 
-showNarrations(stories[0]);
+// showNarrations(stories[0]);
 
 function showOptions(object) {
     for (let answer in object.answer) {
         const button = document.createElement('button')
-        button.classList.add('btn')
-        // button.addEventListener('click')
+        button.classList.add('op-btn')
         optionButtons.appendChild(button)
         console.log(object.answer[answer])
         button.innerText = object.answer[answer]
     }
+
+    showOptions(stories[0]);
 }
 
-showOptions(stories[0]);
-
-
-/*
-function showOptions(object) {
-    console.log(object.answers)
-    optionButtons.innertext = object.answer.option1
+function giveOption (selected) {
+    selectOptions.push(selected)
+    currentStoryIndex += 1 
+    showNarrations(stories[currentStoryIndex]);
 }
-*/
+for (let optionBtn of selectButton) {
+    optionBtn.addEventListener("click", function(event) {
+        const id = event.target.getAttribute("data-id")
+        giveOption(id)
+    })
+}
+
+
+showNarrations(stories[currentStoryIndex]);
 
 
 
--- assets/js/story.js
+++ assets/js/story.js
@@ -2,11 +2,19 @@
 
 var stories = [
     { 
-        narration1: "In the light of the moon a little egg lay on a leaf. One Sunday morning the warm sun came up- crack!",
+        narration: "In the light of the moon a little egg lay on a leaf. One Sunday morning the warm sun came up- crack!",
         answer: {
           option1: "The egg was hatching",
           option2: "The leaf fell from the tree"
         }
-    
-    }      
+    },     
+
+    {
+        narration: "On Tuesday, Mr. Catipillar...",
+        answer: {
+          option1: "Ate TWO pairs",
+          option2: "You saw a slippery slug"
+        }
+
+    }
 ]
