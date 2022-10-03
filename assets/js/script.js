23790695873cace021774246ebd54109d6988b31
JJBR13 <jjbrown1308@gmail.com>
2022-10-03 14:21:49 +0000

    Add option function for story

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 const optionContainer = document.getElementById('answer-btn')
 const startButton = document.getElementById('start-btn')
-const selectButton = document.getElementsByClassName('op-btn')
+const selectButtons = optionContainer.getElementsByClassName('op-btn')
 const narrationStoryLine = document.getElementById('story-segment')
 
 
@@ -15,6 +15,17 @@ function startGame () {
     startButton.classList.add('hide');
     narrationStoryLine.classList.remove('hide');
     optionContainer.classList.remove('hide');
+    
+    let index = 0;
+    for (let optionBtn of selectButtons) {
+        optionBtn.innerHTML = stories[index].answer.option1;
+        optionBtn.addEventListener("click", function(event) {
+            const id = event.target.getAttribute("data-id")
+            giveOption(id)
+        })
+        index++
+    }
+    showNarrations(stories[currentStoryIndex]);
 }
 
 // Displaying start Story 
@@ -24,33 +35,9 @@ function showNarrations(object) {
     narrationStoryLine.innerText = object.narration
 }
 
-// showNarrations(stories[0]);
-// now not working as changed optionButtons to selectButton
-function showOptions(object) {
-    for (let answer in object.answer) {
-        const button = document.createElement('button')
-        button.classList.add('op-btn')
-        optionButtons.appendChild(button)
-        console.log(object.answer[answer])
-        button.innerText = object.answer[answer]
-    }
-
-    showOptions(stories[0]);
-}
-
 function giveOption (selected) {
     selectOptions.push(selected)
     currentStoryIndex += 1 
     showNarrations(stories[currentStoryIndex]);
 }
-for (let optionBtn of selectButton) {
-    optionBtn.addEventListener("click", function(event) {
-        const id = event.target.getAttribute("data-id")
-        giveOption(id)
-    })
-}
-
-showNarrations(stories[currentStoryIndex]);
-
-
 
