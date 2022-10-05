290eae6009b001a18b42ddf91bb2a21a18a08126
JJBR13 <jjbrown1308@gmail.com>
2022-10-05 17:51:54 +0000

    Add imgSelect function to trigger img load

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ const startButton = document.getElementById('start-btn')
 const selectButtons = optionContainer.getElementsByClassName('op-btn')
 const narrationStoryLine = document.getElementById('story-segment')
 const instructions = document.getElementById('instructions')
+const imageArea = document.getElementById('image-div')
 
 let currentStoryIndex = 0;
 let selectOptions = [];
@@ -27,6 +28,7 @@ function advanceStory(){
     if(currentStoryIndex < stories.length){
         showNarrations(stories[currentStoryIndex]);
         selectButtons[0].innerHTML = stories[currentStoryIndex].answer.option1;
+        imgSelect();
         currentStoryIndex++; 
     }
     else{
@@ -40,4 +42,19 @@ function advanceStory(){
 function showNarrations(object) {
   //console.log(object.narration)
   narrationStoryLine.innerText = object.narration;
+}
+
+// Images pop up on click 
+
+function imgSelect() {
+    
+    if (selectButtons[0].innerText == "Ate ONE apple") {
+      let image_element = document.getElementById('image_id')
+
+      image_element.src = '/assets/img/illustration/apple1.png'
+      imageArea.classList.remove('hide')
+      console.log('image_element')
+    } else {
+        console.log('nope')
+    }
 }
--- assets/js/story.js
+++ assets/js/story.js
@@ -4,24 +4,21 @@ var stories = [
     { 
         narration: "In the light of the moon a little egg lay on a leaf. One Sunday morning the warm sun came up- crack!",
         answer: {
-          option1: "The egg was hatching",
-          // option2: "The leaf fell from the tree"
+          option1: "The egg was hatching"
         }
     },     
 
     {
       narration: "On Monday, Mr. Caterpillar...",
       answer: {
-        option1: "Ate ONE apple",
-        // option2: "You saw a slippery slug"
+        option1: "Ate ONE apple"
       }
 
     },
     {
         narration: "On Tuesday, Mr. Caterpillar...",
         answer: {
-          option1: "Ate TWO pairs",
-          // option2: "You saw a slippery slug"
+          option1: "Ate TWO pairs"
         }
 
     },
@@ -29,32 +26,28 @@ var stories = [
     {
       narration: "But was still Hungry. On Wednesday, Mr. Caterpillar... ",
       answer: {
-        option1: "Ate through THREE plums",
-        // option2: "Decided to take a NAP inside the juicey plums"
+        option1: "Ate through THREE plums"
       }
     }, 
 
     {
       narration: "But was still HUNGRY. On Thursday Mr. Caterpillar... ",
       answer: {
-        option1: "Ate FOUR stawberries",
-        // option2: "Saw glowing golden box inside the stawberries "
+        option1: "Ate FOUR stawberries"
       }
     },
 
     {
       narration: "But was still HUNGRY! On Friday, Mr. Caterpillar... ",
       answer: {
-        option1: "Ate through FIVE oranges",
-        // option2: "Saw a slug and asked 'How are you?'"
+        option1: "Ate through FIVE oranges"
       }
     },
     
     {
       narration: "On Saturday Mr. Caterpillar... ",
       answer: {
-        option1: "Was still HUNGRY! He ate through, one piece of chocolate cake, one ice cream, one pickle, one slice of swiss cheese, one slice of salami, one lolipop, one piece of cherry pie, one sausauge, one cupcake, and one slice of watermelon ",
-        // option2: "Saw a pig lying upside down...."
+        option1: "Was still HUNGRY! He ate through, one piece of chocolate cake, one ice cream, one pickle, one slice of swiss cheese, one slice of salami, one lolipop, one piece of cherry pie, one sausauge, one cupcake, and one slice of watermelon"
       }
     }
 ]
